277b0824e6ab28bc290f395ad5ecf6806dde8447
Camilo160605 <camilo1665ober@gmail.com>
2023-11-27 16:03:18 -0500

    Edit and create heroes has been adjusted

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { switchMap } from 'rxjs';
+
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -6,12 +13,65 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit{
+
+  public heroForm = new FormGroup ({
+    id:         new FormControl<string>(''),
+    superhero:  new FormControl<string>('', { nonNullable: true }),
+    publisher:  new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego:  new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img:    new FormControl(''),
+  })
 
   public publishers = [
-    { id: 'Dc Comics',value: 'Dc-Comics'},
-    {id:'Marvel Comics',value : 'Marvel-Comics'}
+    { id: 'Dc Comics',desc: 'Dc - Comics'},
+    {id:'Marvel Comics',desc : 'Marvel - Comics'}
   ]
+  constructor(
+    private heroesService : HeroesService,
+    private activatedRoute : ActivatedRoute,
+    private router : Router,
+    ){}
+    
+    get currentHero() : Hero{
+      const hero = this.heroForm.value as Hero
+      return hero;
+    }
 
+    ngOnInit(): void {
+      if (!this.router.url.includes('edit')) return
+
+      this.activatedRoute.params
+      .pipe(
+        switchMap( ({id})=> this.heroesService.getHeroById(id)),
+      ).subscribe ( hero => {
+        if (!hero) {
+          return this.router.navigateByUrl('/');
+        }
+        this.heroForm.reset(hero)
+        console.log(hero);
+        
+        return
+      })
+    }
+
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
+
+    if (this.currentHero.id ){
+      this.heroesService.updateHero ( this.currentHero )
+      .subscribe ( hero => {
+        //TODO: mostrar snackbar
+
+      })
+    }
+    this.heroesService.addHero(this.currentHero)
+    .subscribe( hero => {
+      //TODO: mostrar snackbar y navegar a /heroes/edit/ hero.id
+    })
+
+  }
 
 }
